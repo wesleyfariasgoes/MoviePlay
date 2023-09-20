6d65369d0ac04851fce6516df291dcebd95ff1ec
wesleygoes <wfariasgoes@gmail.com>
2023-09-20 08:14:57 -0300

    Configura container de navegação do projeto

--- app/src/main/java/com/wfghc/movieplay/MainActivity.kt
+++ app/src/main/java/com/wfghc/movieplay/MainActivity.kt
@@ -2,10 +2,24 @@ package com.wfghc.movieplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.wfghc.movieplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(setOf())
     }
 }
