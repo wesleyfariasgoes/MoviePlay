5d628a58a98c6bded5c8954fa9389c115c585796
wesleygoes <wfariasgoes@gmail.com>
2023-10-25 10:03:49 -0300

    Criação AuthorizationIntercept

---
+++ app/src/main/java/com/wfghc/marvelapp/framework/di/NetworkModule.kt
@@ -0,0 +1,74 @@
+package com.wfghc.marvelapp.framework.di
+
+import com.wfghc.core.data.network.AuthorizationInterceptor
+import com.wfghc.marvelapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.Calendar
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val TIMEOUT_SECOND = 15L
+
+    @Provides
+    fun provideLogginInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            )
+        }
+    }
+
+    @Provides
+    fun providesAuthorizationInterceptor() : AuthorizationInterceptor {
+        return AuthorizationInterceptor(
+            publicKey = BuildConfig.PUBLIC_KEY,
+        privateKey = BuildConfig.PRIVATE_KEY,
+            calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        )
+    }
+
+    @Provides
+    fun provideOkHttpClient (
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authorizationInterceptor)
+            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonCoverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+}
